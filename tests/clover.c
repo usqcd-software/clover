54d100fc7637c78fe4e2478c89581b3e7fec75b9
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2009-06-01 03:45:19 +0000

    Clover code fixed.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/clover/trunk@815 65d2813f-f11f-0410-b17b-8647a28db151

--- tests/clover.c
+++ tests/clover.c
@@ -9,8 +9,8 @@
  **  o----------->
  **
  **
- **     C
- **   D   B
+ **     C               3 0
+ **   D   B             2 1
  **     A
  */
 
@@ -18,7 +18,7 @@ void
 clover(QDP_ColorMatrix *cl[], QDP_ColorMatrix *U[])
 {
     int mu, nu, i;
-    QDP_ColorMatrix *t[5];
+    QDP_ColorMatrix *t[6];
 
     create_Mvector(t, NELEMS(t));
 
@@ -26,39 +26,31 @@ clover(QDP_ColorMatrix *cl[], QDP_ColorMatrix *U[])
         for (nu = mu + 1; nu < NDIM; nu++, i++) {
             /* [0]: B */
             QDP_M_eq_sM(t[0], U[nu], QDP_neighbor[mu], QDP_forward, QDP_all);
+            /* [1]: DA */
+            QDP_M_eq_Ma_times_M(t[1], U[nu], U[mu], QDP_all);
+            /* [2]: DAB */
+            QDP_M_eq_M_times_M(t[2], t[1], t[0], QDP_all);
+            /* [3]: DAB|nu */
+            QDP_M_eq_sM(t[3], t[2], QDP_neighbor[nu], QDP_backward, QDP_all);
+            /* [4]: DABC|nu = p1 */
+            QDP_M_eq_M_times_Ma(t[4], t[3], U[mu], QDP_all);
             /* [1]: C */
             QDP_M_eq_sM(t[1], U[mu], QDP_neighbor[nu], QDP_forward, QDP_all);
+            /* [5]: CDAB|nu */
+            QDP_M_eq_Ma_times_M(t[5], U[mu], t[3], QDP_all);
             /* [2]: BC */
             QDP_M_eq_M_times_Ma(t[2], t[0], t[1], QDP_all);
             /* [3]: BCD */
             QDP_M_eq_M_times_Ma(t[3], t[2], U[nu], QDP_all);
-            /* [4]: ABCD */
-            QDP_M_eq_M_times_M(t[4], U[mu], t[3], QDP_all); /* cl */
-            /* [5]: BCDA */
-            QDP_M_eq_M_times_M(t[5], t[3], U[mu], QDP_all);
-            /* [6]: BCDA|mu */
-            QDP_M_eq_sM(t[6], t[5], QDP_neighbor[mu], QDP_backward, QDP_all);
-            /* [4]: ABCD+BCDA|mu */
-            QDP_M_peq_M(t[4], t[6], QDP_all); /* cl */
-            /* [3]: DA */
-            QDP_M_eq_Ma_times_M(t[3], U[nu], U[mu], QDP_all);
-            /* [5]: DABC */
-            QDP_M_eq_M_times_M(t[5], t[3], t[2], QDP_all);
-            /* [6]: DABC|nu */
-            QDP_M_eq_sM(t[6], t[5], QDP_neighbor[nu], QDP_backward, QDP_all);
-            /* [4]: ABCD+BCDA|mu+DABC|nu */
-            QDP_M_peq_M(t[4], t[6], QDP_all); /* cl */
-            /* [2]: CDA */
-            QDP_M_eq_Ma_times_M(t[2], t[1], t[3], QDP_all);
-            /* [5]: CDAB */
-            QDP_M_eq_M_times_M(t[5], t[2], t[0], QDP_all);
-            /* [6]: CDAB|mu */
-            QDP_M_eq_sM(t[6], t[5], QDP_neighbor[mu], QDP_backward, QDP_all);
-            /* [3]: CDAB|mu|nu */
-            QDP_M_eq_sM(t[3], t[6], QDP_neighbor[nu], QDP_backward, QDP_all);
-            /* [4]: ABCD+BCDA|mu+DABC|nu+CDAB|mu|nu */
-            QDP_M_peq_M(t[4], t[6], QDP_all);
-            /* cl[i]: [4]-[4]^+ */
+            /* [4]: ABCD + DABC|nu */
+            QDP_M_peq_M_times_M(t[4], U[mu], t[3], QDP_all);
+            /* [5]: BCDA + CDAB|nu */
+            QDP_M_peq_M_times_M(t[5], t[3], U[mu], QDP_all);
+            /* [2]: BCDA|mu + CDAB|nu|mu */
+            QDP_M_eq_sM(t[2], t[5], QDP_neighbor[mu], QDP_backward, QDP_all);
+            /* [4]: clover */
+            QDP_M_peq_M(t[4], t[2], QDP_all);
+            
             QDP_M_eq_M(cl[i], t[4], QDP_all);
             QDP_M_meq_conj_M(cl[i], t[4], QDP_all);
         }
