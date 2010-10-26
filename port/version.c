fc904216185aa68e151c06e0cb278523dd0942b4
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2010-10-26 17:09:20 +0000

    Clover 1.2.2 final cut in the trunk.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/clover/trunk@1825 65d2813f-f11f-0410-b17b-8647a28db151

--- port/version.c
+++ port/version.c
@@ -2,10 +2,11 @@
 
 /* Return version of the CLOVER library
  *  log: ready for eigCG deflation.
+ *       1.2.2 final cut
  */
 
 const char *
 Q(version)(void)
 {
-  return "CLOVER version 1.2.2-XXX $Id$";
+  return "CLOVER version 1.2.2 $Id$";
 }
