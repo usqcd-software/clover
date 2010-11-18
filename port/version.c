bfb3031f4ee92df578ba47200a1efc4af260e3bc
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2010-11-18 15:56:16 +0000

    Release 1.2.3 marked and frozen.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/clover/releases/clover-1.2.3@1863 65d2813f-f11f-0410-b17b-8647a28db151

--- port/version.c
+++ port/version.c
@@ -1,12 +1,11 @@
 #include <clover.h>
 
 /* Return version of the CLOVER library
- *  log: ready for eigCG deflation.
- *       1.2.2 final cut
+ *       1.2.3 final cut
  */
 
 const char *
 Q(version)(void)
 {
-  return "CLOVER version 1.2.2 $Id$";
+  return "CLOVER version 1.2.3 $Id$";
 }
