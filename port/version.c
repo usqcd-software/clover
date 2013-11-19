f05d4f4514e275c9313d106d0c804fe40cc97230
Andrew Pochinsky <avp@mit.edu>
2013-11-19 10:44:58 -0500

    version numbers updated and marked

--- port/version.c
+++ port/version.c
@@ -3,10 +3,11 @@
 /* Return version of the CLOVER library
  *  log: ready for eigCG deflation.
  *       1.2.2 final cut
+ *       1.3 series
  */
 
 const char *
 Q(version)(void)
 {
-  return "CLOVER version 1.2.2 $Id$";
+  return "CLOVER version 1.3 XXX";
 }
